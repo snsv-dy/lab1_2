fa93c4fd0d8a3e0e5532d2d75f656dbef4cfa37d
Stasiak.Jacek <222579@edu.p.lodz.pl>
2021-03-19 16:40:21 +0100

    5. Dodano interfejs obliczający podatek i użyto go w metodzie issuance.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,35 +21,36 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(InvoiceRequest request) {
+    public Invoice issuance(InvoiceRequest request, TaxCalculator tax_method) {
         Invoice invoice = Invoice.create(Id.generate(), request.getClient());
 
         for (RequestItem item : request.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
-            String desc = null;
+//            String desc = null;
 
-            switch (item.getProductData().getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
+//            switch (item.getProductData().getType()) {
+//                case DRUG:
+//                    ratio = BigDecimal.valueOf(0.05);
+//                    desc = "5% (D)";
+//                    break;
+//                case FOOD:
+//                    ratio = BigDecimal.valueOf(0.07);
+//                    desc = "7% (F)";
+//                    break;
+//                case STANDARD:
+//                    ratio = BigDecimal.valueOf(0.23);
+//                    desc = "23%";
+//                    break;
+//
+//                default:
+//                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
+//            }
+//
+//            Money taxValue = net.multiplyBy(ratio);
 
-                default:
-                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
+//            Tax tax = new Tax(taxValue, desc);
+            Tax tax = tax_method.getAmount(item.getProductData(), net);
 
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -0,0 +1,8 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public interface TaxCalculator {
+    Tax getAmount(ProductData product, Money cost);
+}
